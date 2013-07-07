066678e51e40b3fcb1fa132bb8f384e730f1b540
Ubuntu <ubuntu@ip-172-31-6-150.eu-west-1.compute.internal>
2013-07-07 14:42:58 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-
+var file="index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', function (err, data) {
+  fs.readFile(file, function (err, data) {
     if (err) throw err;
-    console.log(data);
-    //response.send(data);
+    //console.log(data);
+    response.send('data');
   });
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
